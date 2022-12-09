ed1dce0e64aa14c3583fb0c2d5c136a13b4f2fa1
olena <olena.senyk.pz.2020@lpnu.ua>
2022-12-09 15:45:36 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SenykEkzamen_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.TimeTextBox = new System.Windows.Forms.TextBox();
+            this.FrequencyTextBox = new System.Windows.Forms.TextBox();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.FirstBtn = new System.Windows.Forms.Button();
+            this.LastBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.PrevBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(393, 24);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView.Size = new System.Drawing.Size(379, 381);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_RowHeaderMouseDoubleClick);
+            this.dataGridView.DoubleClick += new System.EventHandler(this.dataGridView_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddBtn.Location = new System.Drawing.Point(83, 256);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(100, 34);
+            this.AddBtn.TabIndex = 2;
+            this.AddBtn.Text = "Add";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameTextBox.Location = new System.Drawing.Point(154, 56);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(210, 34);
+            this.NameTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(20, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(20, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Frequency";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DescriptionTextBox.Location = new System.Drawing.Point(154, 100);
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(210, 34);
+            this.DescriptionTextBox.TabIndex = 7;
+            // 
+            // TimeTextBox
+            // 
+            this.TimeTextBox.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimeTextBox.Location = new System.Drawing.Point(154, 144);
+            this.TimeTextBox.Name = "TimeTextBox";
+            this.TimeTextBox.Size = new System.Drawing.Size(210, 34);
+            this.TimeTextBox.TabIndex = 8;
+            // 
+            // FrequencyTextBox
+            // 
+            this.FrequencyTextBox.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FrequencyTextBox.Location = new System.Drawing.Point(154, 188);
+            this.FrequencyTextBox.Name = "FrequencyTextBox";
+            this.FrequencyTextBox.Size = new System.Drawing.Size(210, 34);
+            this.FrequencyTextBox.TabIndex = 9;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DeleteBtn.Location = new System.Drawing.Point(201, 256);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(100, 34);
+            this.DeleteBtn.TabIndex = 10;
+            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // FirstBtn
+            // 
+            this.FirstBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FirstBtn.Location = new System.Drawing.Point(83, 300);
+            this.FirstBtn.Name = "FirstBtn";
+            this.FirstBtn.Size = new System.Drawing.Size(100, 34);
+            this.FirstBtn.TabIndex = 11;
+            this.FirstBtn.Text = "First";
+            this.FirstBtn.UseVisualStyleBackColor = true;
+            this.FirstBtn.Click += new System.EventHandler(this.FirstBtn_Click);
+            // 
+            // LastBtn
+            // 
+            this.LastBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LastBtn.Location = new System.Drawing.Point(201, 300);
+            this.LastBtn.Name = "LastBtn";
+            this.LastBtn.Size = new System.Drawing.Size(100, 34);
+            this.LastBtn.TabIndex = 12;
+            this.LastBtn.Text = "Last";
+            this.LastBtn.UseVisualStyleBackColor = true;
+            this.LastBtn.Click += new System.EventHandler(this.LastBtn_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NextBtn.Location = new System.Drawing.Point(83, 344);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(100, 34);
+            this.NextBtn.TabIndex = 13;
+            this.NextBtn.Text = "Next";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
+            // 
+            // PrevBtn
+            // 
+            this.PrevBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PrevBtn.Location = new System.Drawing.Point(201, 344);
+            this.PrevBtn.Name = "PrevBtn";
+            this.PrevBtn.Size = new System.Drawing.Size(100, 34);
+            this.PrevBtn.TabIndex = 14;
+            this.PrevBtn.Text = "Previous";
+            this.PrevBtn.UseVisualStyleBackColor = true;
+            this.PrevBtn.Click += new System.EventHandler(this.PrevBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 430);
+            this.Controls.Add(this.PrevBtn);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.LastBtn);
+            this.Controls.Add(this.FirstBtn);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.FrequencyTextBox);
+            this.Controls.Add(this.TimeTextBox);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.TextBox TimeTextBox;
+        private System.Windows.Forms.TextBox FrequencyTextBox;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button FirstBtn;
+        private System.Windows.Forms.Button LastBtn;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Button PrevBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SenykEkzamen_
+{
+    public partial class Form1 : Form
+    {
+        EkzKPZ model;
+
+        public Form1()
+        {
+            InitializeComponent();
+            model = new EkzKPZ();
+            dataGridView.DataSource = model.Calendar.ToList();
+            SetBoxes("", "", "", "", "Add", false);
+        }
+
+        private void SetBoxes(string _name, string _desc, string _time, string _freq, string _addBtn, bool _enable)
+        {
+            NameTextBox.Text = _name;
+            DescriptionTextBox.Text = _desc;
+            TimeTextBox.Text = _time;
+            FrequencyTextBox.Text = _freq;
+            AddBtn.Text = _addBtn;
+            DeleteBtn.Enabled = _enable;
+        }
+
+        private string[] GetDataFromTextBoxes()
+        {
+            return new string[] 
+            { 
+                NameTextBox.Text.Trim(), 
+                DescriptionTextBox.Text.Trim(), 
+                TimeTextBox.Text.Trim(),
+                FrequencyTextBox.Text.Trim() 
+            };
+        }
+
+        private void dataGridView_DoubleClick(object sender, EventArgs e)
+        {
+            this.dataGridView.CurrentCell.Selected = false;
+            SetBoxes("", "", "", "", "Add", false);
+        }
+
+        private void dataGridView_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView.CurrentRow.Cells[0].Value); ;
+            Calendar c = model.Calendar.Where(x => x.Id == id).FirstOrDefault();
+            SetBoxes(c.Name, c.Description, c.Time.ToString(), c.Frequency, "Update", true);
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            string _name = GetDataFromTextBoxes()[0];
+            string _desc = GetDataFromTextBoxes()[1];
+            string _time = GetDataFromTextBoxes()[2];
+            string _freq = GetDataFromTextBoxes()[3];
+
+            if (AddBtn.Text == "Add")
+            {
+                model.Calendar.Add(new Calendar { Name = _name, Description = _desc, Time = _time, Frequency = _freq });
+            }
+            else
+            {
+                int id = Convert.ToInt32(dataGridView.CurrentRow.Cells[0].Value); ;
+                Calendar c = model.Calendar.Where(x => x.Id == id).FirstOrDefault();
+                if (c != null)
+                {
+                    c.Name = _name;
+                    c.Description = _desc;
+                    c.Time = _time;
+                    c.Frequency = _freq;
+                    model.Entry(c).State = EntityState.Modified;
+                }
+            }
+            model.SaveChanges();
+            SetBoxes("", "", "", "", "Add", false);
+            dataGridView.DataSource = model.Calendar.ToList();
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView.CurrentRow.Cells[0].Value); ;
+            Calendar c = model.Calendar.Find(id);
+            if (c != null)
+            {
+                model.Entry(c).State = EntityState.Deleted;
+                model.SaveChanges();
+            }
+            SetBoxes("", "", "", "", "Add", false);
+            dataGridView.DataSource = model.Calendar.ToList();
+        }
+
+        private void FirstBtn_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView.Rows[0].Cells[0].Value);
+            Calendar c = model.Calendar.Where(x => x.Id == id).FirstOrDefault();
+            SetBoxes(c.Name, c.Description, c.Time.ToString(), c.Frequency, "Update", true);
+            dataGridView.CurrentCell = dataGridView.Rows[0].Cells[0];
+            PrevBtn.Enabled = false;
+            NextBtn.Enabled = true;
+        }
+
+        private void LastBtn_Click(object sender, EventArgs e)
+        {
+            List<Calendar> list = model.Calendar.ToList();
+            Calendar c = list.First();
+            SetBoxes(c.Name, c.Description, c.Time.ToString(), c.Frequency, "Update", true);
+            dataGridView.CurrentCell = dataGridView.Rows[dataGridView.RowCount - 1].Cells[0];
+            NextBtn.Enabled = false;
+            PrevBtn.Enabled = true;
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+            dataGridView.CurrentCell = dataGridView.Rows[dataGridView.CurrentRow.Index + 1].Cells[0];
+            int id = Convert.ToInt32(dataGridView.CurrentRow.Cells[0].Value); ;
+            Calendar c = model.Calendar.Where(x => x.Id == id).FirstOrDefault();
+            SetBoxes(c.Name, c.Description, c.Time.ToString(), c.Frequency, "Update", true);
+            if (dataGridView.CurrentCell.RowIndex != dataGridView.RowCount - 1)
+            {
+                PrevBtn.Enabled = true;
+                NextBtn.Enabled = true;
+            }
+            else
+            {
+                PrevBtn.Enabled = true;
+                NextBtn.Enabled = false;
+            }
+        }
+
+        private void PrevBtn_Click(object sender, EventArgs e)
+        {
+            dataGridView.CurrentCell = dataGridView.Rows[dataGridView.CurrentRow.Index - 1].Cells[0];
+            int id = Convert.ToInt32(dataGridView.CurrentRow.Cells[0].Value); ;
+            Calendar c = model.Calendar.Where(x => x.Id == id).FirstOrDefault();
+            SetBoxes(c.Name, c.Description, c.Time.ToString(), c.Frequency, "Update", true);
+            if (dataGridView.CurrentCell.RowIndex != 0)
+            {
+                PrevBtn.Enabled = true;
+                NextBtn.Enabled = true;
+            }
+            else
+            {
+                PrevBtn.Enabled = false;
+                NextBtn.Enabled = true;
+            }
+        }
+    }
+}
